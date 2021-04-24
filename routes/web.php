191c18d6341ed94d74a52ec154360a8891a6c73d
DevKish0n <pritu111225@gmail.com>
2021-04-24 15:33:24 +0600

    Auth Aded

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,24 @@ $router->get('/', function () use ($router) {
     return view('home', ['version' => $router->app->version()]);
 });
 
-$router->group(['prefix' => 'api/user'], function () use ($router) {
+$router->group(['prefix' => 'api'], function () use ($router) {
     //Register User
     $router->post('/register', 'AuthController@register');
     //User Login
     $router->post('/login', 'AuthController@login');
-    //User Logout
-    $router->post('/logout', 'AuthController@logout');
-});
 
-$router->group(['prefix' => 'api'], function () use ($router) {
-    //Get all posts
-    $router->get('/posts', 'PostController@index');
-    //Add a new post
-    $router->post('/posts', 'PostController@store');
-    //Update a post
-    $router->put('/posts/{id}', 'PostController@update');
-    //Delete a post
-    $router->delete('/posts/{id}', 'PostController@destroy');
-    //get a single post
-    $router->get('/posts/{id}', 'PostController@single');
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        //User Logout
+        $router->post('/logout', 'AuthController@logout');
+        //Get all posts
+        $router->get('/posts', 'PostController@index');
+        //Add a new post
+        $router->post('/posts', 'PostController@store');
+        //Update a post
+        $router->put('/posts/{id}', 'PostController@update');
+        //Delete a post
+        $router->delete('/posts/{id}', 'PostController@destroy');
+        //get a single post
+        $router->get('/posts/{id}', 'PostController@single');
+    });
 });
